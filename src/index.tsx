1233999e3e94d4c0df3ce49a6921677343b09842
Ana Pereira <anaferraspereira@gmail.com>
2021-06-07 10:52:50 +0100

    Configure css loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './index.css';
 
 const App = () => {
     return <h1>This is my React app!</h1>;
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
   // the app entry point is /src/index.tsx
   entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
-  	// the output of the webpack build will be in /public directory
-    path: path.resolve(__dirname, 'public'),
+  	// the output of the webpack build will be in /build directory
+    path: path.resolve(__dirname, 'build'),
     // the filename of the JS bundle will be bundle.js
     filename: 'bundle.js'
   },
@@ -28,6 +28,10 @@ module.exports = {
           // attach the presets to the loader (most projects use .babelrc file instead)
           presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       }
     ]
   },
@@ -35,8 +39,10 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
-      contentBase: path.join(__dirname,'public')
+      contentBase: path.join(__dirname,'build'),
+      port: 3000,
+      open: true
   },
   // add a custom index.html as the template
-  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })]
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
 };
